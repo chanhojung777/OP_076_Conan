b617fc79df329eed8f7ae0976e3828b4ad535a58
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-06-23 16:22:14 +0900

    dd

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -99,6 +99,7 @@ bool hyundai_forward_bus1 = false;
 
 static int hyundai_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
 
+#define NULL ((void*)0)
   //bool valid = addr_safety_check(to_push, hyundai_rx_checks, HYUNDAI_RX_CHECK_LEN,
   //                               hyundai_get_checksum, hyundai_compute_checksum,
   //                               hyundai_get_counter);
