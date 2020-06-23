1b239a2690a46282f59ea857728d95eea2d943e4
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-06-23 11:00:34 +0900

    safety modify

--- panda/board/safety.h
+++ panda/board/safety.h
@@ -9,6 +9,7 @@
 #include "safety/safety_gm.h"
 #include "safety/safety_ford.h"
 #include "safety/safety_hyundai.h"
+//#include "safety/safety_hyundai_073.h"
 #include "safety/safety_chrysler.h"
 #include "safety/safety_subaru.h"
 #include "safety/safety_mazda.h"
