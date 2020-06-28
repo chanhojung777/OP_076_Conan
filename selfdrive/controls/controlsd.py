073ff4a242d2e89dffdab90af90a8915da483dde
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-06-28 20:50:01 +0900

    dd

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -11,6 +11,7 @@ import cereal.messaging as messaging
 from selfdrive.config import Conversions as CV
 from selfdrive.boardd.boardd import can_list_to_can_capnp
 from selfdrive.car.car_helpers import get_car, get_startup_event
+from selfdrive.car.hyundai.interface import CarInterface
 from selfdrive.controls.lib.lane_planner import CAMERA_OFFSET
 from selfdrive.controls.lib.drive_helpers import update_v_cruise, initialize_v_cruise
 from selfdrive.controls.lib.longcontrol import LongControl, STARTING_TARGET_SPEED
@@ -561,7 +562,8 @@ class Controls:
 
     if self.read_only:
       self.hyundai_lkas = self.read_only
-    elif CS.cruiseState.enabled:
+    elif CS.cruiseState.enabled and self.hyundai_lkas:
+      self.CP = CarInterface.live_tune( self.CP, False )      
       self.hyundai_lkas = False
     
 
