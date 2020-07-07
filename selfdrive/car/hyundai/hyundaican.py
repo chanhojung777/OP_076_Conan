9d3391040b94605cca16bd3316c8f3b61e582989
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-07-07 20:45:37 +0900

    dd

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -69,6 +69,9 @@ def create_clu11(packer, frame, clu11, button, speed = None ):
   if speed != None:
     values["CF_Clu_Vanz"] = speed
 
+  values["CF_Clu_CruiseSwState"] = button
+  values["CF_Clu_AliveCnt1"] = frame % 0x10
+
   return packer.make_can_msg("CLU11", 0, values)
 
 
