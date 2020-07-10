dc13adfa0ef79f70e450e0b8b8e92f4332fc15cd
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-07-10 18:38:38 +0900

    dd

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -164,7 +164,7 @@ FEATURES = {
   "use_elect_gears": [CAR.KIA_OPTIMA_H, CAR.KONA_EV, CAR.GRANDEUR_HYBRID], # Use TCU Message for Gear Selection
 }
 
-EV_HYBRID = [CAR.IONIQ_EV_LTD, CAR.IONIQ, CAR.KONA_EV,CAR.GRANDEUR_HYBRID]
+EV_HYBRID = [CAR.KONA_EV,CAR.GRANDEUR_HYBRID]
 
 DBC = {
   CAR.ELANTRA: dbc_dict('hyundai_kia_generic', None),
