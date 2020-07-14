94430cf7f0091c20d93c4ff0650a8d632a34f505
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-07-14 18:40:30 +0900

    dd

--- panda/board/safety/safety_hyundai_076.h
+++ panda/board/safety/safety_hyundai_076.h
@@ -7,8 +7,8 @@ const int HYUNDAI_DRIVER_TORQUE_ALLOWANCE = 50;
 const int HYUNDAI_DRIVER_TORQUE_FACTOR = 2;
 const int HYUNDAI_STANDSTILL_THRSLD = 30;  // ~1kph
 const CanMsg HYUNDAI_TX_MSGS[] = {
-  {832, 0, 8}, {832, 1, 8}, // LKAS11 Bus 0, 1
-  {1265, 0, 4}, {1265, 1, 4}, {1265, 2, 4}, // CLU11 Bus 0, 1, 2
+  {832, 0, 8}, //{832, 1, 8}, // LKAS11 Bus 0, 1
+  {1265, 0, 4}, //{1265, 1, 4}, {1265, 2, 4}, // CLU11 Bus 0, 1, 2
 //  {1157, 0, 4}, // LFAHDA_MFC Bus 0
   {593, 2, 8},  // MDPS12, Bus 2
  // {1056, 0, 8}, //   SCC11,  Bus 0
