18beee54a35667e42aaa6fdbbfe20d4375b60ef1
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-09-09 21:13:54 +0900

    dd

--- selfdrive/controls/lib/driver_monitor.py
+++ selfdrive/controls/lib/driver_monitor.py
@@ -15,11 +15,11 @@ EventName = car.CarEvent.EventName
 # ******************************************************************************************
 
 _AWARENESS_TIME = 70.  # one minute limit without user touching steering wheels make the car enter a terminal status
-_AWARENESS_PRE_TIME_TILL_TERMINAL = 15.  # a first alert is issued 25s before expiration
-_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.  # a second alert is issued 15s before start decelerating the car
+_AWARENESS_PRE_TIME_TILL_TERMINAL = 15.  # a first alert is issued 25s(default 15) before expiration
+_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.  # a second alert is issued 15s(default 6) before start decelerating the car
 _DISTRACTED_TIME = 11.
-_DISTRACTED_PRE_TIME_TILL_TERMINAL = 5.
-_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 3.
+_DISTRACTED_PRE_TIME_TILL_TERMINAL = 7.
+_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 2.
 
 _FACE_THRESHOLD = 0.4
 _EYE_THRESHOLD = 0.6
@@ -46,7 +46,7 @@ _POSE_OFFSET_MAX_COUNT = 3600 # stop deweighting new data after 6 min, aka "shor
 _RECOVERY_FACTOR_MAX = 5. # relative to minus step change
 _RECOVERY_FACTOR_MIN = 1.25 # relative to minus step change
 
-MAX_TERMINAL_ALERTS = 10 # not allowed to engage after 3 terminal alerts
+MAX_TERMINAL_ALERTS = 20 # not allowed to engage after 3 terminal alerts
 MAX_TERMINAL_DURATION = 300 # 30s
 
 # model output refers to center of cropped image, so need to apply the x displacement offset
